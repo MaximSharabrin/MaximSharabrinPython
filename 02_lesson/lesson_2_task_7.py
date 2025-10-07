3fd275214718b3a5baa0f3fa3fc28b1b269bb9ff
sharabrin <celson@yandex.ru>
2025-10-07 23:25:05 +0400

    Add lesson 2

---
+++ 02_lesson/lesson_2_task_7.py
@@ -0,0 +1,3 @@
+
+new_list = list(range(18, -2, -4))
+print(new_list)
--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -2,3 +2,7 @@ first_name = input('Введите вашу фамилию ')
 last_name = input('Введите ваше имя ')
 
 print('Вас зовут ' + first_name + ' ' + last_name)
+
+1
+2
+3
